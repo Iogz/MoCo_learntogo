a28eee0b8ec4a4f812b3bc33e83721232949849d
Iogz <93346244+Iogz@users.noreply.github.com>
2023-02-24 20:06:52 +0100

    HomeViewModel

--- app/src/main/java/com/example/moco_learntogo/MainActivity.kt
+++ app/src/main/java/com/example/moco_learntogo/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.moco_learntogo.data.location.LocationDetails
 import com.example.moco_learntogo.ui.theme.MoCo_learntogoTheme
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
@@ -28,6 +30,10 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
 
+                    val context = LocalContext.current
+                    var currentLocation by remember {
+                        mutableStateOf(LocationDetails(0.toDouble(), 0.toDouble()))
+                    }
 
 
 
